2e2199dd33e66e7908644b605caf35575dd545f7
Josef Würf <sepp2357@yahoo.de>
2023-10-23 21:03:40 +0200

    Adjusted the tests for the deriver to demand string equality of the derivatives instead of just checking for the type. Also added one new test for the analyzer for the case when no variable is contained within the expression.

--- tests/analyzer.test.js
+++ tests/analyzer.test.js
@@ -3,7 +3,6 @@ import { Deriver } from "../src/deriver.js";
 import { Lexer } from "../src/lexer.js";
 import { Parser } from "../src/parser.js";
 
-//TODO: Make the test conditions stricter
 const lexer = new Lexer();
 const parser = new Parser();
 const analyzer = new Analyzer();
@@ -21,13 +20,20 @@ test("Analyzer: x^(y/z)-k+x", () => {
   expect(result).toEqual(new Set(["x", "y", "z", "k"]));
 });
 
+test("Analyzer: 17-23^2", () => {
+  const ast = parseInput("17-23^2");
+  const result = ast.accept(analyzer);
+  expect(result).toEqual(new Set());
+});
+
 test("Deriver: x+y", () => {
   const ast = parseInput("x+y");
   const variables = ast.accept(analyzer);
   deriver.variables = variables;
   const result = ast.accept(deriver);
-  expect(typeof result.derivatives["x"]).toBe("string");
-  expect(typeof result.derivatives["y"]).toBe("string");
+  expect(result.exprString).toBe("x+y");
+  expect(result.derivatives["x"]).toBe("1+0");
+  expect(result.derivatives["y"]).toBe("0+1");
 });
 
 test("Deriver: x*y", () => {
@@ -35,8 +41,9 @@ test("Deriver: x*y", () => {
   const variables = ast.accept(analyzer);
   deriver.variables = variables;
   const result = ast.accept(deriver);
-  expect(typeof result.derivatives["x"]).toBe("string");
-  expect(typeof result.derivatives["y"]).toBe("string");
+  expect(result.exprString).toBe("x*y");
+  expect(result.derivatives["x"]).toBe("x*0+1*y");
+  expect(result.derivatives["y"]).toBe("x*1+0*y");
 });
 
 test("Deriver: x/y", () => {
@@ -44,8 +51,9 @@ test("Deriver: x/y", () => {
   const variables = ast.accept(analyzer);
   deriver.variables = variables;
   const result = ast.accept(deriver);
-  expect(typeof result.derivatives["x"]).toBe("string");
-  expect(typeof result.derivatives["y"]).toBe("string");
+  expect(result.exprString).toBe("x/y");
+  expect(result.derivatives["x"]).toBe("(1*y-x*0)/y^2");
+  expect(result.derivatives["y"]).toBe("(0*y-x*1)/y^2");
 });
 
 test("Deriver: -(2+17)*x", () => {
@@ -53,15 +61,25 @@ test("Deriver: -(2+17)*x", () => {
   const variables = ast.accept(analyzer);
   deriver.variables = variables;
   const result = ast.accept(deriver);
-  expect(typeof result.derivatives["x"]).toBe("string");
+  expect(result.exprString).toBe("-(2+17)*x");
+  expect(result.derivatives["x"]).toBe("-(2+17)*1+(0+0)*(-1)*x");
 });
 
-test.only("Deriver: exp(x+y)", () => {
+test("Deriver: exp(x+y)", () => {
   const ast = parseInput("exp(x+y)");
   const variables = ast.accept(analyzer);
   deriver.variables = variables;
   const result = ast.accept(deriver);
-  console.log(result);
-  expect(typeof result.derivatives["x"]).toBe("string");
-  expect(typeof result.derivatives["y"]).toBe("string");
+  expect(result.exprString).toBe("exp(x+y)");
+  expect(result.derivatives["x"]).toBe("exp(x+y)*(1+0)");
+  expect(result.derivatives["y"]).toBe("exp(x+y)*(0+1)");
+});
+
+test("Deriver: asin(-x)", () => {
+  const ast = parseInput("asin(-x)");
+  const variables = ast.accept(analyzer);
+  deriver.variables = variables;
+  const result = ast.accept(deriver);
+  expect(result.exprString).toBe("asin(-x)");
+  expect(result.derivatives["x"]).toBe("1/(1-(-x)^2)^0.5*(1*(-1))");
 });
